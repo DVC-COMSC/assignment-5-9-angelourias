456c91e5fd2b4d18ae78f53aadbebb887ec28b7a
angelourias <angelou02005@gmail.com>
2024-03-08 21:38:52 -0800

    hw

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	int prev, current;
-	/* ****************************
-	// Complete this code.
-	*************************** */
+int main() {
+    int prev, current;
+
+    cout << "Enter a value: ";
+    cin >> prev;
+
+    while (true) {
+        cout << "Enter a value: ";
+        cin >> current;
+
+        if (current <= prev) {
+            cout << current << " ";
+        } else {
+            cout << endl;
+            break;
+        }
+
+        prev = current;
+    }
+
+    return 0;
 }
